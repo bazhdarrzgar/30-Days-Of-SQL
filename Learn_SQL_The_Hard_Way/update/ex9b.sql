04f98fcd15437222fa304a810299248f36306dc3
bazhdarrzgar <soyansoon9@gmail.com>
2022-09-18 17:26:45 +0300

    30 Days Of SQL

--- Learn_SQL_The_Hard_Way/update/ex9b.sql
+++ Learn_SQL_The_Hard_Way/update/ex9b.sql
@@ -1,7 +1,51 @@
-
--- updating table (pet) using id or in another word using PRIMARY KEY of this table
+-- you can't use sub statement in update or insert or delete just you can do that in select
 update pet set name = 'Deceased' where dead in (
-	where id = 1
+	select id from pet where id = 2;
 );
 
-select * from pet where id = 2;
+-- ERROR 1093 (HY000): You can't specify target table 'pet' for update in FROM clause
+
+
+
+
+
+-- you can't use sub statement in update or insert or delete just you can do that in select
+update pet set name = 'Deceased' where dead in (
+	where id = 2;
+);
+
+-- ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ')' at line 1
+
+
+
+---------------------------------------------------------------------
+
+
+-- using sub statement with select
+SELECT * FROM pet WHERE dead IN (
+	SELECT id FROM pet
+);
+
++----+-------------+-------+------+------+
+| id | name        | breed | age  | dead |
++----+-------------+-------+------+------+
+|  1 | Fancy pants | Robot |    1 |    1 |
++----+-------------+-------+------+------+
+
+
+
+---------------------------------------------------------------------
+
+
+
+-- updating table (pet) using id or in another word using PRIMARY KEY of this table
+update pet set name = 'Deceased' where dead = 1;
+
+
+select * from pet where id = 1;
+
++----+----------+-------+------+------+
+| id | name     | breed | age  | dead |
++----+----------+-------+------+------+
+|  1 | Deceased | Robot |    1 |    1 |
++----+----------+-------+------+------+
