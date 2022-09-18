be771e3b79414cb72250df9afa282a124bb78ff1
bazhdarrzgar <soyansoon9@gmail.com>
2022-09-18 18:11:20 +0300

    30 Days Of SQL

--- Learn_SQL_The_Hard_Way/update/ex10.sql
+++ Learn_SQL_The_Hard_Way/update/ex10.sql
@@ -1,12 +1,48 @@
-select * from pet;
+-- first take ready your work
+insert into person_pet values(0, 0, NULL);
+insert into pet values (0, 'Zaka', 'Robot', 1, 1);
+insert into person values (0 'Zed', 'Shaw', 37, NULL, NULL);
 
-update pet set name = "Zed's pet" where id in(
-	select pet.id
-	from pet, person_pet, person
-	where
-	person.id = person_pet.person_id and
-	pet.id = person_pet.pet_id and
-	person.first_name = 'Zed'
-);
 
-select * from pet;
+
+-- before update
+select * from pet where id = 0;
+
++----+------+-------+------+------+
+| id | name | breed | age  | dead |
++----+------+-------+------+------+
+|  0 | Zaka | Robot |    1 |    1 |
++----+------+-------+------+------+
+
+
+
+-- finding my target
+select pet.id from pet, person_pet, person
+where 
+  person.id = person_pet.person_id and
+  pet.id = person_pet.pet_id and
+  person.first_name = 'Zed';
+
++----+
+| id |
++----+
+|  0 |
++----+
+
+
+
+-- updating my target
+update pet set name = "Zed's pet" where id = 0;
+
+
+
+
+-- before update
+select * from pet where id = 0;
+
++----+-----------+-------+------+------+
+| id | name      | breed | age  | dead |
++----+-----------+-------+------+------+
+|  0 | Zed's pet | Robot |    1 |    1 |
++----+-----------+-------+------+------+
+
--- Learn_SQL_The_Hard_Way/update/ex10a.sql
+++ Learn_SQL_The_Hard_Way/update/ex10a.sql
@@ -1,12 +1,36 @@
-select * from pet;
+-- before update
+select * from pet where id = 0;
 
-update pet set name = "Zed's dead pet" where id in(
-	select person_pet.pet_id
-	from pet, person_pet, person
-	where
++----+-----------+-------+------+------+
+| id | name      | breed | age  | dead |
++----+-----------+-------+------+------+
+|  0 | Zed s pet | Robot |    1 |    1 |
++----+-----------+-------+------+------+
+
+
+
+-- get my target
+select pet.id from pet, person_pet, person
+where
 	person.id = person_pet.person_id and
 	pet.id = person_pet.pet_id and
-	person.first_name = 'Zed'
-);
+	person.first_name = 'Zed';
 
-select * from pet;
++----+
+| id |
++----+
+|  0 |
++----+
+
+
+
+-- after update
+update pet set name = "Zed's dead pet" where id = 0;
+
+select * from pet where id = 0;
+
++----+----------------+-------+------+------+
+| id | name           | breed | age  | dead |
++----+----------------+-------+------+------+
+|  0 | Zed s dead pet | Robot |    1 |    1 |
++----+----------------+-------+------+------+
